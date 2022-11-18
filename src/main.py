f0cf3ed6e94110eb3bd74afcfa6a24324c0393c7
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-11-18 15:41:32 +0100

    updated main

--- src/main.py
+++ src/main.py
@@ -4,5 +4,7 @@ Main file for the course FYS-2021 Machine Learning, UiT The Arctic University.
 
 __auther__ = 'Daniel Elisabethsønn Antonsen' 
 
-
+# Importing modules and libraries
+import numpy as np
+from matplotlib import pyplot as plt
 
