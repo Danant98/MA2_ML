a4999c00c3affac3eafbafb58382c0ada4951651
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-11-27 18:33:09 +0100

    updated file

--- src/KMC.py
+++ src/KMC.py
@@ -6,99 +6,60 @@ __author__ = 'Daniel Elisabethsønn Antonsen'
 
 # Importing libraries and modules
 import numpy as np
+import matplotlib.pyplot as plt
 
-## OBS! THIS CODE HAS BEEN WRITTEN USING GITHUB CO-PILOT (SKRIV OM FØR INNLEVERING!)
-class K_means:
+class k_mean:
 
     def __init__(self, X:np.ndarray, k:int):
         """
-        Args:
-            X: np.ndarray, the data matrix
-            k: int, the number of clusters
+        Args: 
+            X: np.ndarray, matrix containg unlabeld dataset
+            k: int, number of clusters
         """
         self.__X = X
-        self.__k = k
-        self.__N = X.shape[0]
-        self.__d = X.shape[1]
-        self.__centroids = np.random.rand(k, self.__d)
-        self.__clusters = np.zeros(self.__N)
-        self.__distances = np.zeros((self.__N, self.__k))
-        self.__new_centroids = np.zeros((self.__k, self.__d))
-        self.__old_centroids = np.zeros((self.__k, self.__d))
-        self.__converged = False
+        self._k = k
+        self._centroids = X[np.random.choice(X.shape[0], k, replace=False)]
+        self._labels = np.zeros(X.shape[0])
+        self._distance = np.zeros((X.shape[0], k))
 
-    def __distance(self, X:np.ndarray, centroids:np.ndarray):
+    def _comp_distance(self):
         """
-        Method for computing the distance between the data points and the centroids
-
-        Args:
-            X: np.ndarray, the data matrix
-            centroids: np.ndarray, the centroids
-
-        Output:
-            np.ndarray, array containing the distances
+        Method for computing the Euclidean distance matrix
         """
-        return np.sqrt(np.sum((X - centroids[:, np.newaxis])**2, axis=2))
+        for i in range(self._k):
+            self._distance[:, i] = np.linalg.norm(self.__X - self._centroids[i, :], axis=1)
+    
+    def _assign(self):
+        """
+        Method for assigning points to clusters
+        """
+        self._labels = np.argmin(self._distance, axis=1)
 
-    def __update_centroids(self):
+    def _update_centroids(self):
         """
         Method for updating the centroids
         """
-        for i in range(self.__k):
-            self.__new_centroids[i] = np.mean(self.__X[self.__clusters == i], axis=0)
+        for i in range(self._k):
+            self._centroids[i] = np.mean(self.__X[self._labels == i], axis=0)
 
-    def __update_clusters(self):
-        """
-        Method for updating the clusters
-        """
-        self.__clusters = np.argmin(self.__distances, axis=1)
-
-    def __convergence(self):
-        """
-        Method for checking if the algorithm has converged
-        """
-        if np.all(self.__new_centroids == self.__old_centroids):
-            self.__converged = True
-
-    def __update(self):
-        """
-        Method for updating the centroids and clusters
-        """
-        self.__update_centroids()
-        self.__update_clusters()
-        self.__convergence()
-
-    def __assign_clusters(self):
-        """
-        Method for assigning the clusters
-        """
-        self.__clusters = np.argmin(self.__distances, axis=1)
-
-    def __assign_centroids(self):
-        """
-        Method for assigning the centroids
-        """
-        self.__centroids = self.__X[np.random.choice(self.__N, self.__k, replace=False)]
-
-    def __assign(self):
-        """
-        Method for assigning the clusters and centroids
-        """
-        self.__assign_clusters()
-        self.__assign_centroids()
-
-    def __compute(self):
-        """
-        Method for computing the distances between the data points and the centroids
-        """
-        self.__distances = self.__distance(self.__X, self.__centroids)
-    
     def run(self):
         """
-        Method for running the k-means clustering algorithm
+        Method for running the algorithm
         """
-        self.__assign()
-        while not self.__converged:
-            self.__compute()
-            self.__update()
-        return self.__clusters, self.__centroids
+        while True:
+            self._comp_distance()
+            self._assign()
+            self._update_centroids()
+            old_distance = self._distance
+            if np.all(np.linalg.norm(self._distance - old_distance)) < 1E-5:
+                break
+        return self._labels, self._centroids
+
+    def _plot(self):
+        """
+        Method for plotting the clusters
+        """
+        plt.figure(figsize=(10, 8))
+
+
+
--- src/main.py
+++ src/main.py
@@ -9,18 +9,30 @@ import numpy as np
 import os 
 from MDS import Multidimensional_scaling
 import pandas as pd
+from KMC import k_mean
+import matplotlib.pyplot as plt
 
 ## Problem 1
 # Opening data containing the B-matrix
 data_inner_sweden = np.loadtxt(os.path.join("resources", "city-inner-sweden.csv"))
 names_inner_sweden = pd.read_csv(os.path.join("resources", "city-names-sweden.csv"), header=None).to_numpy()
+faces_data = np.loadtxt(os.path.join("resources", "frey-faces.csv"))
 
 # Running multidimensional scaling algorithm
-Multidimensional_scaling(data_inner_sweden, names_inner_sweden).run()
+#Multidimensional_scaling(data_inner_sweden, names_inner_sweden).run()
 
+labels_2, centroids_2 = k_mean(faces_data, 2).run()
 
-
-
+im = centroids_2[0, :]
+im2 = centroids_2[1, :]
+im = np.reshape(im, (28, 20))
+im2 = np.reshape(im2, (28, 20))
+plt.gray()
+plt.figure()
+plt.imshow(im)
+plt.figure()
+plt.imshow(im2)
+plt.show()
 if __name__ == "__main__":
     pass   
 
